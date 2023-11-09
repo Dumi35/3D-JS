dda36365abb540f66a82054a6739b170b3976053
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2023-11-10 00:00:27 +0100

    Light and controls

--- main.js
+++ main.js
@@ -1,82 +1,94 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import * as dat from "dat.gui"
+
+const Sizes={
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight(0xffffff, 50)
-light.position.set(0.8, 1.4, 1.0)
-scene.add(light)
+const sphereGeometry = new THREE.SphereGeometry(3,64,64) //3 is radius, 64 and 64 are x an dy segments
 
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color:"#00ff83",
+    //wireframe: true
+})
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.set(0.8, 1.4, 1.0)
+//using meshBasicMaterial doesn't require you to add light source
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+// mesh is combo of geometry and material
+const sphereMesh = new THREE.Mesh(sphereGeometry,sphereMaterial)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
+const planeGeometry = new THREE.PlaneGeometry(30, 30)
+const planeMaterial = new THREE.MeshStandardMaterial({
+    color:"#ffffff",
+})
 
-//const material = new THREE.MeshNormalMaterial()
+const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial)
 
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    'house_002.fbx',
-    (object) => {
-        // object.traverse(function (child) {
-        //     if ((child as THREE.Mesh).isMesh) {
-        //         // (child as THREE.Mesh).material = material
-        //         if ((child as THREE.Mesh).material) {
-        //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
-        //         }
-        //     }
-        // })
-        // object.scale.set(.01, .01, .01)
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+planeMesh.position.set(0,0,0)
+planeMesh.rotation.x = -0.5 * Math.PI
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+const gui = new dat.GUI()
+
+const options = {
+    sphereColor: "#ffea00",
+    wireframe: false
 }
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
+gui.addColor(options, "sphereColor").onChange((e )=>{
+    sphereMesh.material.color.set(e)
+})
+gui.add(options, "wireframe").onChange((e)=>{
+    sphereMesh.material.wireframe=e
+})
 
-function animate() {
+const gridHelper = new THREE.GridHelper(30,50)
+scene.add(gridHelper)
+
+const boxGeometry = new THREE.BoxGeometry(10,5,10)
+const boxMaterial = new THREE.MeshStandardMaterial({color:"red"})
+
+const boxMesh = new THREE.Mesh(boxGeometry,boxMaterial)
+//scene.add(boxMesh)
+
+//lights
+const light = new THREE.PointLight(0xffffff,200,100) //color, intensity, distance, decay(amount light dims)
+light.position.set(10,10,10)
+//scene.add(light)
+
+const dLight = new THREE.DirectionalLight(0xffffff,0.8)
+dLight.position.set(10,10,10)
+const dLightHelper = new THREE.DirectionalLightHelper(dLight,5)
+scene.add(dLight, dLightHelper)
+
+const camera = new THREE.PerspectiveCamera(45, Sizes.width/Sizes.height, 0.1,1000) //parameters are FOV FIELD OF VIEW in degrees, the wider the camera focal length, the more distorted the image looks, the aspect ratio of camera, that is width, then height, then near and far of camera
+camera.position.set(0, 10,20)
+
+const axesHelper = new THREE.AxesHelper(30)
+
+
+scene.add(camera, axesHelper)
+scene.add(sphereMesh, planeMesh)
+
+//Renderer
+const canvas = document.querySelector(".webgl") 
+const renderer = new THREE.WebGL1Renderer({canvas})
+
+renderer.setSize(Sizes.width, Sizes.height)
+document.body.appendChild( renderer.domElement );
+//orbit control
+
+const orbit = new OrbitControls(camera, renderer.domElement)
+
+
+function animate(){
     requestAnimationFrame(animate)
-
-    controls.update()
-
-    render()
-
-    stats.update()
-}
-
-function render() {
+    orbit.update()
     renderer.render(scene, camera)
 }
 
 animate()
+
